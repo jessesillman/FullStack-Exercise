1e5ff6f91e937afcdbe5f79b9b25cfeb11d02238
jessemikalo <jesse.sillman@yahoo.com>
2024-01-08 17:39:09 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
 document.getElementById("actionButton").addEventListener("click", function() {
-    alert("Button clicked!");
+    var description = document.getElementById("description");
+    if (description.style.display === "none") {
+        description.style.display = "block";
+    } else {
+        description.style.display = "none";
+    }
 });
